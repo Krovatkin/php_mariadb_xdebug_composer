5a4837002f0bc3e5f0f2b1eb89f1b21d97fb3a44
Nikolay Korovaiko <korovaikon@gmail.com>
2022-04-17 00:23:32 -0700

    connect to db

--- html/index.php
+++ html/index.php
@@ -1,5 +1,7 @@
 <?php
-xdebug_break();
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+//xdebug_break();
 $a = 2;
 $a += 2;
 
---
+++ html/test_mysql.php
@@ -0,0 +1,21 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+$mysqli = new mysqli("localhost", "my_user", "my_password", "world");
+
+/* check connection */
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+
+/* check if server is alive */
+if ($mysqli->ping()) {
+    printf ("Our connection is ok!\n");
+} else {
+    printf ("Error: %s\n", $mysqli->error);
+}
+
+/* close connection */
+$mysqli->close();
+?>
